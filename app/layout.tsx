0e38bae3602df048cb1a8adf53d9b6a9dd87abd3
Matt <mattjackson2023@outlook.com>
2024-12-22 09:27:10 +0000

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Standard Favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* Android Chrome Icons */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="min-h-screen bg-gray-50">
         {children}
